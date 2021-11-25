aaacea4fc362e3c330e44384cea0c8b833012e1f
Alejandro <alex.galez2218@gmail.com>
2021-11-25 10:39:55 +0100

    Modificación con compañeros

--- src/com/acing/Producto.java
+++ src/com/acing/Producto.java
@@ -1,11 +1,6 @@
 package com.acing;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
-public class Producto implements List<Producto>, Comerciable {
+public class Producto implements Comerciable {
 
 	private char id;
 	private String marcaProducto;
@@ -74,123 +69,6 @@ public class Producto implements List<Producto>, Comerciable {
 		return getDescripcionProducto() + " - " + getMarcaProducto();
 	}
 
-	// Todos los de abajo
-
-	@Override
-	public int size() {
-		return 0;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return false;
-	}
-
-	@Override
-	public boolean contains(Object o) {
-		return false;
-	}
-
-	@Override
-	public Iterator<Producto> iterator() {
-		return null;
-	}
-
-	@Override
-	public Object[] toArray() {
-		return null;
-	}
-
-	@Override
-	public <T> T[] toArray(T[] a) {
-		return null;
-	}
-
-	@Override
-	public boolean add(Producto e) {
-		return false;
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		return false;
-	}
-
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		return false;
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends Producto> c) {
-		return false;
-	}
-
-	@Override
-	public boolean addAll(int index, Collection<? extends Producto> c) {
-		return false;
-	}
-
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		return false;
-	}
-
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		return false;
-	}
-
-	@Override
-	public void clear() {
-
-	}
-
-	@Override
-	public Producto get(int index) {
-		return null;
-	}
-
-	@Override
-	public Producto set(int index, Producto element) {
-		return null;
-	}
-
-	@Override
-	public void add(int index, Producto element) {
-
-	}
-
-	@Override
-	public Producto remove(int index) {
-		return null;
-	}
-
-	@Override
-	public int indexOf(Object o) {
-		return 0;
-	}
-
-	@Override
-	public int lastIndexOf(Object o) {
-		return 0;
-	}
-
-	@Override
-	public ListIterator<Producto> listIterator() {
-		return null;
-	}
-
-	@Override
-	public ListIterator<Producto> listIterator(int index) {
-		return null;
-	}
-
-	@Override
-	public List<Producto> subList(int fromIndex, int toIndex) {
-		return null;
-	}
-
 	@Override
 	public String getDescripcion() {
 		return null;
@@ -200,4 +78,7 @@ public class Producto implements List<Producto>, Comerciable {
 	public float getPrecio() {
 		return 0;
 	}
+
+	// Todos los de abajo
+
 }
